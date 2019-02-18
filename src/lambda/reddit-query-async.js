ca3cf629ca21ba03416758a5e4c537432e4ee565
Marcell Schuh <marcell.schuh@gmail.com>
2019-02-18 19:53:42 +0100

    Reddit Qurry function v0

---
+++ src/lambda/reddit-query-async.js
@@ -0,0 +1,49 @@
+import fetch from 'node-fetch';
+
+const base_url = 'https://api.reddit.com/search?q=smart%20fridge';
+
+export async function handler(event, context) {
+
+  try {
+    console.log(event)
+    console.log(event.body)
+    const body = JSON.parse(event.body);
+    
+    console.log(body.last ? `${base_url}&after=${body.last}` : base_url)
+
+    const response = await fetch(body.last ? `${base_url}&after=${body.last}` : base_url);
+
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText };
+    }
+    const data = await response.json();
+
+
+    let resArr = [];
+    let last = "";
+    data.data.children.forEach(post => {
+        let postToAdd = {
+            'title': post.data.title,
+            'url': post.data.url,
+            'permalink': post.data.permalink,
+            'name': post.data.name
+        };
+        if(postToAdd.url) resArr.push(postToAdd);
+    });
+
+    last = resArr[resArr.length-1].name;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({posts: resArr, last: last})
+    };
+  } catch (err) {
+    console.log(err); // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+    };
+  }
+
+}
